55c32894dc25633fe8c49e696c4468b6b3917c37
Birendra Patro <birendra.patro@ascensive.in>
2024-12-31 11:31:13 +0530

    Nikul- Monthly Dep change

--- afya_care/models/account_asset.py
+++ afya_care/models/account_asset.py
@@ -18,6 +18,7 @@ class AccountAsset(models.Model):
         store=True,
         help="The date on which the asset's depreciation will end.",
     )
+    afy_monthly_depreciation_value = fields.Float(string="Monthly Depreciation Value")
 
     @api.depends('method_number', 'prorata_date', 'method_period')
     def _compute_depreciation_end_date(self):
@@ -27,3 +28,22 @@ class AccountAsset(models.Model):
                 asset.depreciation_end_date = asset.prorata_date + relativedelta(months=duration_months)
             else:
                 asset.depreciation_end_date = False
+
+    def compute_depreciation_board(self, date=False):
+        self.ensure_one()
+
+        # Need to unlink draft move before adding new one because if we create new move before, it will cause an error
+        # in the compute for the depreciable/cumulative value
+        self.depreciation_move_ids.filtered(lambda mv: mv.state == 'draft' and (mv.date >= date if date else True)).unlink()
+        new_depreciation_moves_data = self._recompute_board(date)
+        new_depreciation_moves = self.env['account.move'].create(new_depreciation_moves_data)
+        # Added code for Monthly Depreciation Value Calculate
+        self.afy_monthly_depreciation_value = (
+            new_depreciation_moves[1].amount_total
+            if len(new_depreciation_moves) > 1
+            else (new_depreciation_moves[0].amount_total if new_depreciation_moves else 0.0)
+        )
+        if self.state == 'open':
+            # In case of the asset is in running mode, we post in the past and set to auto post move in the future
+            new_depreciation_moves._post()
+        return True
