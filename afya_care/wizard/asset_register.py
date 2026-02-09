dc00d844e06036dedbd84592ce06ddfc4fbb05d9
Birendra Patro <birendra.patro@ascensive.in>
2026-02-09 12:59:28 +0530

    Palak-AK25022121-Detailed Fixed Asset Register

---
+++ afya_care/wizard/asset_register.py
@@ -0,0 +1,101 @@
+from odoo import models, fields
+from io import BytesIO
+import xlsxwriter
+from datetime import date
+import base64
+from calendar import monthrange
+
+class AssetRegisterReport(models.TransientModel):
+    _name = 'report.account.asset.register'
+    _description = 'Asset Register Report'
+
+    date_to = fields.Date(string='End Date', required=True)
+    company_id = fields.Many2one('res.company', string='Company', required=True, default=lambda self: self.env.company)
+
+    def action_generate_report(self):
+        assets = self.env['account.asset'].search([
+            ('company_id', '=', self.company_id.id),
+            ('acquisition_date', '<=', self.date_to)
+        ])
+
+        headers = [
+            'Reporting Date','Asset Original Cost Account', 'Depreciation Expense Account', 'Accum Depreciation Account',
+            'Label', 'Original cost', 'Already Depreciated Amount', 'Accum Depreciation as at end of last financial year',
+            'Current depreciation', 'Closing NBV', 'Status', 'Disposal value', 'Dep Start',
+            'Dep End', 'Total Months', 'Location'
+        ]
+
+        output = BytesIO()
+        workbook = xlsxwriter.Workbook(output)
+        sheet = workbook.add_worksheet("Asset Register")
+        bold = workbook.add_format({'bold': True})
+
+        for col, header in enumerate(headers):
+            sheet.write(0, col, header, bold)
+
+        prev_year = self.date_to.year - 1
+        prev_year_end = date(prev_year, 12, 31)
+        
+        last_day_of_month = monthrange(self.date_to.year, self.date_to.month)[1]
+        is_last_date_of_month = self.date_to.day == last_day_of_month
+
+        if is_last_date_of_month:
+            curr_year_end = self.date_to
+        else:
+            prev_month = self.date_to.month - 1
+            if prev_month == 0:
+                prev_month = 1
+            last_day = monthrange(self.date_to.year, prev_month)[1]
+            curr_year_end = date(self.date_to.year, prev_month, last_day)
+
+        for row, asset in enumerate(assets, start=1):
+            asset_acc = asset.original_move_line_ids[:1].account_id.name if asset.original_move_line_ids else ''
+            depr_lines = asset.depreciation_move_ids.filtered(lambda d: d.date)
+            depr_prev_year = sum(depr_lines.filtered(lambda d: d.date == prev_year_end).mapped('asset_depreciated_value'))
+            depr_current_year = sum(depr_lines.filtered(lambda d: d.date == curr_year_end).mapped('asset_depreciated_value'))
+            current_depr = depr_current_year - depr_prev_year
+            if current_depr < 0:
+                current_depr = 0.0
+            closing_nbv = asset.original_value - (depr_current_year + (asset.already_depreciated_amount_import or 0.0))
+            
+            total_months = asset.method_number * (12 if asset.method_period == 'year' else 1)
+
+            values = [
+                self.date_to.strftime('%d/%m/%Y'),  # Reporting Date
+                asset_acc,
+                asset.account_depreciation_expense_id.code,
+                asset.account_depreciation_id.code,
+                asset.name,
+                asset.original_value,
+                asset.already_depreciated_amount_import or 0.0,
+                depr_prev_year,
+                current_depr,
+                closing_nbv,
+                asset.state,
+                asset.afy_disposal_date.strftime('%d/%m/%Y') if asset.afy_disposal_date else '',
+                asset.prorata_date.strftime('%d/%m/%Y') if asset.prorata_date else '',
+                asset.depreciation_end_date.strftime('%d/%m/%Y') if asset.depreciation_end_date else '',
+                total_months,
+                asset.location_id.name or '',
+            ]
+
+            for col, val in enumerate(values):
+                sheet.write(row, col, val if val is not None else '')
+
+        workbook.close()
+        output.seek(0)
+
+        attachment = self.env['ir.attachment'].create({
+            'name': f'Asset Register Report - {self.date_to.strftime("%d-%m-%Y")}.xlsx',
+            'type': 'binary',
+            'datas': base64.b64encode(output.read()),
+            'res_model': self._name,
+            'res_id': self.id,
+            'mimetype': 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+        })
+
+        return {
+            'type': 'ir.actions.act_url',
+            'url': f'/web/content/{attachment.id}?download=true',
+            'target': 'new',
+        }
